0b3fbccdb75421e6af1bce2fb335e03abf00f379
lolekmakso <makstarasovv1@gmail.com>
2023-11-25 23:21:37 +0200

    added slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ const menuMobile = document.querySelector(".header__menu-list");
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu--open");
 });
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+  spaceBetween: 2,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperTwo = new Swiper(".certificates__slider", {
+  loop: true,
+  slidesPerView: 2.35,
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
