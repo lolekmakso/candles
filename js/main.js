4db64b0014819726b6f982e84f066a3d1d845b7c
lolekmakso <makstarasovv1@gmail.com>
2023-11-27 16:50:06 +0200

    added accordeon

--- js/main.js
+++ js/main.js
@@ -22,4 +22,32 @@ const swiperTwo = new Swiper(".certificates__slider", {
   pagination: {
     el: ".swiper-pagination",
   },
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2.35,
+  //   },
+  //   480: {
+  //     slidesPerView: 2,
+  //   },
+  //   360:{
+  //     slidesPerView: 1,
+  //   }
+  // },
+});
+
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = accordeon.querySelectorAll(".accordeon__title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener("click", function () {
+    const currentText =
+      accordeonTitle.parentElement.querySelector(".accordeon__text");
+    accordeonTitle.classList.toggle("accordeon__title--active");
+    currentText.classList.toggle("accordeon__text--visible");
+    if (currentText.classList.contains("accordeon__text--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  });
 });
